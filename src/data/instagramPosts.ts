2b0f686c5d2fd8124a51b1f1aaa04957f23043a4
Marcos Monge <marcosmonge5@gmail.com>
2026-09-05 17:32:32 +0000

    Usar publicUrl() para que las imágenes fijas funcionen tanto en /Cercadillo/ como en el dominio propio

--- src/data/instagramPosts.ts
+++ src/data/instagramPosts.ts
@@ -1,3 +1,4 @@
+import { publicUrl } from '../utils/publicUrl';
 export type InstagramPost = {
   id: string;
   url: string;
@@ -15,7 +16,7 @@ export const instagramPosts: InstagramPost[] = [
   {
     id: 'poesia',
     url: 'https://www.instagram.com/infocercadillo/reel/DclkoW6soF5/',
-    image: '/images/instagram/reel-poesia.jpg',
+    image: publicUrl('/images/instagram/reel-poesia.jpg'),
     caption: 'Simplemente, poesía ✨',
     date: '28 ago 2026',
     kind: 'reel',
@@ -23,7 +24,7 @@ export const instagramPosts: InstagramPost[] = [
   {
     id: 'corzo',
     url: 'https://www.instagram.com/infocercadillo/reel/DcTtxtYCec5/',
-    image: '/images/instagram/reel-corzo.jpg',
+    image: publicUrl('/images/instagram/reel-corzo.jpg'),
     caption: 'Aquí, tal vez, haya oro. Pero lo que sí que hay es vida. Hay paisajes, hay pueblos, hay futuro.',
     date: '21 ago 2026',
     kind: 'reel',
@@ -31,7 +32,7 @@ export const instagramPosts: InstagramPost[] = [
   {
     id: 'atardecer-iglesia',
     url: 'https://www.instagram.com/infocercadillo/p/Db_z-_fAnlG/',
-    image: '/images/instagram/atardecer-iglesia.jpg',
+    image: publicUrl('/images/instagram/atardecer-iglesia.jpg'),
     caption: 'No pudo haber mejor momento ni lugar que Cercadillo. Nuestro pueblo.',
     date: '13 ago 2026',
     kind: 'post',
@@ -39,7 +40,7 @@ export const instagramPosts: InstagramPost[] = [
   {
     id: 'banco-fuente-soto',
     url: 'https://www.instagram.com/infocercadillo/p/DbtnvdROyPQ/',
-    image: '/images/instagram/banco-fuente-soto.jpg',
+    image: publicUrl('/images/instagram/banco-fuente-soto.jpg'),
     caption: 'Nuevo banco en la Fuente del Soto, para seguir disfrutando del pueblo en verano.',
     date: '6 ago 2026',
     kind: 'post',
@@ -47,7 +48,7 @@ export const instagramPosts: InstagramPost[] = [
   {
     id: 'incendio-mierla',
     url: 'https://www.instagram.com/infocercadillo/p/DbIfY3JsrBw/',
-    image: '/images/instagram/incendio-mierla.jpg',
+    image: publicUrl('/images/instagram/incendio-mierla.jpg'),
     caption: 'El incendio de Mierla golpeó muy cerca de Cercadillo. Toca trabajar más duro, pero levantaremos esta tierra una vez más.',
     date: '23 jul 2026',
     kind: 'post',
@@ -55,7 +56,7 @@ export const instagramPosts: InstagramPost[] = [
   {
     id: 'reel-mina',
     url: 'https://www.instagram.com/infocercadillo/reel/DZ70MTiMPip/',
-    image: '/images/instagram/reel-mina.jpg',
+    image: publicUrl('/images/instagram/reel-mina.jpg'),
     caption: 'El pueblo está más vivo de lo que creen: informarse, resolver dudas y defender el territorio.',
     date: '23 jun 2026',
     kind: 'reel',
